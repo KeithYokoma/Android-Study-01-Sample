5437d1a6afe528758e158d6d8d8549f3d4e4c2c9
Keishin Yokomaku <keishin.yokomaku@mixi.co.jp>
2012-11-02 18:31:22 +0900

    Fix main activity test and compose activity test

--- test/src/jp/mixi/androidstudy01/test/MainActivityTest.java
+++ test/src/jp/mixi/androidstudy01/test/MainActivityTest.java
@@ -3,35 +3,93 @@ package jp.mixi.androidstudy01.test;
 import android.app.Activity;
 import android.app.Instrumentation.ActivityMonitor;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.KeyEvent;
+import android.widget.EditText;
 
 import com.jayway.android.robotium.solo.Solo;
 
 import jp.mixi.androidstudy01.MainActivity;
+import jp.mixi.androidstudy01.R;
 import jp.mixi.androidstudy01.diary.DiaryComposeActivity;
 
+/**
+ * A functionality test for main activity.
+ * @author keishin.yokomaku
+ */
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
     public MainActivityTest() {
         super(MainActivity.class);
     }
 
-    public void testCallMenu() {
+    public void testAddAndDeleteEntry() throws Exception {
         Activity activity = getActivity();
-        // Set activity monitor to watch activity behaviour
+
+        // Set up activity monitor to watch activity behaviour
         ActivityMonitor monitor = new ActivityMonitor(DiaryComposeActivity.class.getName(), null, false);
         getInstrumentation().addMonitor(monitor);
 
         // Robotium test framework
-        Solo solo = new Solo(getInstrumentation(), activity);
-        // Show options menu by send tapping menu key event
-        solo.sendKey(Solo.MENU);
-        // Search a text and tap view that contains the text
-        solo.clickOnText(activity.getString(jp.mixi.androidstudy01.R.string.activity_main_menu_add));
+        Solo soloMain = new Solo(getInstrumentation(), activity);
+        // Show options menu by emulate tapping menu key
+        soloMain.sendKey(Solo.MENU);
+        // Tap options menu
+        soloMain.clickOnText(activity.getString(R.string.activity_main_menu_add));
 
-        // Get target activity
-        Activity target = getInstrumentation().waitForMonitorWithTimeout(monitor, 1000);
-        // Target activity has been launched
+        // Get starting activity
+        Activity compose = getInstrumentation().waitForMonitorWithTimeout(monitor, 1000);
+        // Should be hit once
         assertEquals(1, monitor.getHits());
-        // Close the activity to proceed this test
-        if (target != null) target.finish();
+        if (compose == null) fail("did not start compose activity. unexpected situation.");
+
+        // Control compose activity
+        Solo soloCompose = new Solo(getInstrumentation(), compose);
+        // Emulate tapping edit text
+        soloCompose.clickOnEditText(0);
+        // Tapping soft keyboard emulation
+        soloCompose.sendKey(KeyEvent.KEYCODE_T);
+        soloCompose.sendKey(KeyEvent.KEYCODE_S);
+        EditText title = soloCompose.getEditText(0);
+        assertEquals("ts", title.getText().toString());
+
+        // Move to the next edit text
+        soloCompose.clickOnEditText(1);
+        soloCompose.sendKey(KeyEvent.KEYCODE_T);
+        soloCompose.sendKey(KeyEvent.KEYCODE_S);
+        soloCompose.sendKey(KeyEvent.KEYCODE_T);
+        EditText body = soloCompose.getEditText(1);
+        assertEquals("tst", body.getText().toString());
+
+        // Save the diary, and returning it to main activity
+        soloCompose.clickOnButton(compose.getString(R.string.activity_compose_diary_save));
+        assertTrue(compose.isFinishing());
+
+        // Find body text to prove the entity is actually added to the list.
+        assertTrue(soloMain.searchText("tst"));
+
+        // Tap the inserted entity
+        soloMain.clickLongInList(0);
+        // Context menu will be displayed
+        assertTrue(soloMain.searchText(activity.getString(R.string.activity_main_context_menu_delete)));
+        soloMain.clickOnText(activity.getString(R.string.activity_main_context_menu_delete));
+        // Confirmation dialog will be displayed after tapping delete context menu
+        assertTrue(soloMain.searchText(activity.getString(R.string.activity_main_context_menu_delete_title)));
+        assertTrue(soloMain.searchText(activity.getString(R.string.activity_main_context_menu_delete_message)));
+        // Negative button will do nothing
+        soloMain.clickOnText(activity.getString(R.string.activity_main_context_menu_delete_negative));
+
+        // Tap the inserted entity
+        soloMain.clickLongInList(0);
+        // Context menu will be displayed
+        assertTrue(soloMain.searchText(activity.getString(R.string.activity_main_context_menu_delete)));
+        soloMain.clickOnText(activity.getString(R.string.activity_main_context_menu_delete));
+        // Confirmation dialog will be displayed after tapping delete context menu
+        assertTrue(soloMain.searchText(activity.getString(R.string.activity_main_context_menu_delete_title)));
+        assertTrue(soloMain.searchText(activity.getString(R.string.activity_main_context_menu_delete_message)));
+        // Positive button will delete the entity
+        soloMain.clickOnText(activity.getString(R.string.activity_main_context_menu_delete_positive));
+        // Nothing is in the list, so empty view will be visible
+        soloMain.searchText(activity.getString(R.string.activity_main_listview_empty));
+
+        soloMain.finishOpenedActivities();
     }
 }
--- test/src/jp/mixi/androidstudy01/test/diary/DiaryComposeActivityTest.java
+++ test/src/jp/mixi/androidstudy01/test/diary/DiaryComposeActivityTest.java
@@ -3,12 +3,15 @@ package jp.mixi.androidstudy01.test.diary;
 import android.app.Activity;
 import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import jp.mixi.androidstudy01.R;
 import jp.mixi.androidstudy01.diary.DiaryComposeActivity;
 import jp.mixi.androidstudy01.diary.entity.DiaryEntity;
+
+import java.lang.reflect.Field;
 
 /**
  * A functionality test for {@link DiaryComposeActivity}.
@@ -103,5 +106,150 @@ public class DiaryComposeActivityTest extends ActivityInstrumentationTestCase2<D
         assertEquals("testtest", body.getText().toString());
         assertEquals("4 / 20", titleLength.getText().toString());
         assertEquals("8 / 10000", bodyLength.getText().toString());
+    }
+
+    public void testSaveClick() throws Exception {
+        final Activity activity = getActivity();
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                getInstrumentation().callActivityOnStart(activity);
+            }
+        });
+        getInstrumentation().waitForIdleSync();
+
+        final Button save = (Button) activity.findViewById(R.id.ComposeDiarySave);
+        // Controlling ui is not allowed from outside ui thread,
+        // so pass the runnable object and run it on ui thread.
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                // Click the button on ui thread
+                assertTrue(save.performClick());
+            }
+        });
+        getInstrumentation().waitForIdleSync();
+
+        // Activity has been finished.
+        assertTrue(activity.isFinishing());
+
+        try {
+            // Obtain activity result data with reflection! Too complexed!
+            Field f = Activity.class.getDeclaredField("mResultCode");
+            f.setAccessible(true);
+            int actualResultCode = (Integer)f.get(getActivity());
+            // OK result should be returned
+            assertEquals(Activity.RESULT_OK, actualResultCode);
+            f = Activity.class.getDeclaredField("mResultData");
+            f.setAccessible(true);
+            Intent intent = (Intent) f.get(getActivity());
+            DiaryEntity entity = intent.<DiaryEntity>getParcelableExtra(DiaryComposeActivity.RESULT_EXTRA_DIARY_ENTITY);
+            assertNotNull(entity);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException("Looks like the Android Activity class has changed it's   private fields for mResultCode or mResultData.  Time to update the reflection code.", e);
+        }
+    }
+
+    public void testCancelClick() throws Exception {
+        final Activity activity = getActivity();
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                getInstrumentation().callActivityOnStart(activity);
+            }
+        });
+        getInstrumentation().waitForIdleSync();
+
+        final Button cancel = (Button) activity.findViewById(R.id.ComposeDiaryCancel);
+        // Controlling ui is not allowed from outside ui thread,
+        // so pass the runnable object and run it on ui thread.
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                // Click the button on ui thread
+                assertTrue(cancel.performClick());
+            }
+        });
+        getInstrumentation().waitForIdleSync();
+
+        assertTrue(activity.isFinishing());
+
+        try {
+            // Obtain activity result data with reflection! Too complexed!
+            Field f = Activity.class.getDeclaredField("mResultCode");
+            f.setAccessible(true);
+            int actualResultCode = (Integer)f.get(getActivity());
+            // Canceled result should be returned
+            assertEquals(Activity.RESULT_CANCELED, actualResultCode);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException("Looks like the Android Activity class has changed it's   private fields for mResultCode or mResultData.  Time to update the reflection code.", e);
+          }
+    }
+
+    /**
+     * Test all functionality(Start activity, input some text, save them, and finish the activity with result).
+     * @throws Exception
+     */
+    public void testComposeDiary() throws Exception {
+        final Activity activity = getActivity();
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                getInstrumentation().callActivityOnStart(activity);
+            }
+        });
+        getInstrumentation().waitForIdleSync();
+
+        final EditText body = (EditText) activity.findViewById(R.id.ComposeDiaryBodyInput);
+        final TextView bodyLength = (TextView) activity.findViewById(R.id.ComposeDiaryBodyLength);
+        final EditText title = (EditText) activity.findViewById(R.id.ComposeDiaryTitleInput);
+        final TextView titleLength = (TextView) activity.findViewById(R.id.ComposeDiaryTitleLength);
+
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                title.setText("test");
+                body.setText("testtest");
+            }
+        });
+        getInstrumentation().waitForIdleSync();
+
+        assertEquals("test", title.getText().toString());
+        assertEquals("testtest", body.getText().toString());
+        assertEquals("4 / 20", titleLength.getText().toString());
+        assertEquals("8 / 10000", bodyLength.getText().toString());
+
+        final Button save = (Button) activity.findViewById(R.id.ComposeDiarySave);
+        // Controlling ui is not allowed from outside ui thread,
+        // so pass the runnable object and run it on ui thread.
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                // Click the button on ui thread
+                assertTrue(save.performClick());
+            }
+        });
+        getInstrumentation().waitForIdleSync();
+
+        // Activity has been finished.
+        assertTrue(activity.isFinishing());
+
+        try {
+            // Obtain activity result data with reflection! Too complexed!
+            Field f = Activity.class.getDeclaredField("mResultCode");
+            f.setAccessible(true);
+            int actualResultCode = (Integer)f.get(getActivity());
+            // OK result should be returned
+            assertEquals(Activity.RESULT_OK, actualResultCode);
+            f = Activity.class.getDeclaredField("mResultData");
+            f.setAccessible(true);
+            Intent intent = (Intent) f.get(getActivity());
+            DiaryEntity entity = intent.<DiaryEntity>getParcelableExtra(DiaryComposeActivity.RESULT_EXTRA_DIARY_ENTITY);
+            assertNotNull(entity);
+            assertEquals("test", entity.getTitle());
+            assertEquals("testtest", entity.getBody());
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException("Looks like the Android Activity class has changed it's   private fields for mResultCode or mResultData.  Time to update the reflection code.", e);
+        }
     }
 }
